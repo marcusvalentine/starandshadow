769235946a8112ce14e8253e8bb2b15b3bd761af
Simon Key <sjk@psimonkey.org.uk>
2014-07-24 18:36:02 +0100

    Tweaks from prod.

--- starandshadow/wsgi.py
+++ starandshadow/wsgi.py
@@ -13,7 +13,10 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os,sys
+sys.path.append('/opt/ss')
+sys.path.append('/opt/ss/starandshadow')
+sys.path.append('/opt/ss/venv/lib/python2.7/site-packages')
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
